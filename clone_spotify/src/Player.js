c9990ec7d269e4dbccc9937642077ac356e6052e
Harsha <harshasah5@gmail.com>
2024-05-14 23:13:59 +0530

    lost

--- clone_spotify/src/Player.js
+++ clone_spotify/src/Player.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import "./Player.css";
+import Sidebar from "./Sidebar";
 
 function Player({ spotify }) {
     return (
-        <div>
+        <div className="player">
+            <div className="player_body">
+                <Sidebar />
+            </div>
             <h1>Welcome to spotify</h1>
+
         </div>
     );
 }
