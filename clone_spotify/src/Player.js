2804c84047b637b548754f4fc09b2bc34df868cc
Harsha <harshasah5@gmail.com>
2024-05-14 23:37:42 +0530

    2:23:57

--- clone_spotify/src/Player.js
+++ clone_spotify/src/Player.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Player.css";
 import Sidebar from "./Sidebar";
+import Body from './Boody';
 
 function Player({ spotify }) {
     return (
         <div className="player">
             <div className="player_body">
                 <Sidebar />
+                <Body />
             </div>
             <h1>Welcome to spotify</h1>
 
---
+++ clone_spotify/src/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+    return(
+        <div className="sidebar">
+            <h1>I am the sidebar</h1>
+        </div>
+    )
+}
